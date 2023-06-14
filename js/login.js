40fd84adf975eec7cad78e08f1e105578e07e8e3
sujungkang98 <sujungs98@naver.com>
2023-06-14 16:17:25 +0900

    jmkjmkm

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const logBox = [
+    {
+        name:'홍길동',
+        id:'green',
+        pw:'1234',
+        num: "01012345678",
+        email: "green@naver.com"
+    },
+
+    
+    {   name:'홍길순',
+        id:'green2',
+        pw:'1234',
+        num:"01011112222",
+        email: "green02@naver.com"
+    },
+]
+
+export default logBox
+
+
